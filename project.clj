49612bf2438c5398c1f9e85a9a570d27c50308e8
Thomas <introt@gmail.com>
2015-10-14 22:49:13 +0200

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
-(defproject aurman "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject aurman "0.1.0"
+  :description "A simple AUR package downloader"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
